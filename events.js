b7047a3e624e5789f19b7cc24d45e2293c9517e4
◥꧁DaͥrkͣKͫing <93399269+DarkKingbot@users.noreply.github.com>
2021-11-09 17:22:14 +0530

    Update events.js

--- events.js
+++ events.js
@@ -1,2 +1,52 @@
+/* Codded by @Omindu Anjana
 
-Coming Soon...
+
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Whats bot - Omindu Anjana
+
+
+*/
+var config = require('./config');
+var Commands = [];
+
+function newcmdaddtoSMART(info, func) {
+   
+    var types = ['photo', 'image', 'text', 'message'];
+
+    var infos = {
+        fromMe: info['fromMe'] === undefined ? true : info['fromMe'], // Or Sudo
+        onlyGrpSMART: info['onlyGrpSMART'] === undefined ? false : info['onlyGrpSMART'],
+        onlyPinned: info['onlyPinned'] === undefined ? false : info['onlyPinned'],
+        onlyPm: info['onlyPm'] === undefined ? false : info['onlyPm'],
+        delownSMARTcmd: info['delownSMARTcmd'] === undefined ? true : info['delownSMARTcmd'],
+        desc: info['desc'] === undefined ? '' : info['desc'],
+        usage: info['usage'] === undefined ? '' : info['usage'],
+        dontAdCommandList: info['dontAdCommandList'] === undefined ? false : info['dontAdCommandList'],
+        warn: info['warn'] === undefined ? '' : info['warn'],
+        function: func
+    };
+
+    if (info['on'] === undefined && info['pattern'] === undefined) {
+        infos.on = 'message';
+        infos.fromMe = false;
+    } else if (info['on'] !== undefined && types.includes(info['on'])) {
+        infos.on = info['on'];
+
+        if (info['pattern'] !== undefined) {
+            infos.pattern = new RegExp((info['handler'] === undefined || info['handler'] === true ? config.HANDLERS : '') + info.pattern, (info['flags'] !== undefined ? info['flags'] : ''));
+        }
+    } else {
+        infos.pattern = new RegExp((info['handler'] === undefined || info['handler'] === true ? config.HANDLERS : '') + info.pattern, (info['flags'] !== undefined ? info['flags'] : ''));
+    }
+
+    Commands.push(infos);
+    return infos;
+}
+
+module.exports = {
+    newcmdaddtoSMART: newcmdaddtoSMART,
+    commands: Commands
+}

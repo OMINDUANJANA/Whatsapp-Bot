defc85d2db94ca72771f897efdcce16a10b30601
◥꧁DaͥrkͣKͫing <93399269+DarkKingbot@users.noreply.github.com>
2021-11-09 17:15:19 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,2 +1,61 @@
+/* Codded by Omindu Anjana
 
-Coming Soon...
+Facebook: 
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Whats bot - Omindu Anjana
+*/
+
+const chalk = require('chalk');
+const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
+const {StringSession} = require('./smart help/');
+const fs = require('fs');
+
+async function SmartHelp connection();
+    const Session = new StringSession();  
+    conn.version = [2, 2126, 14]
+    conn.logger.level = 'warn';
+    conn.regenerateQRIntervalMs = 50000;
+    
+    conn.on('connecting', async () => {
+        console.log(`${chalk.green.bold('Smart')}${chalk.blue.bold('Help')}
+${chalk.white.italic('SmartHelpString')}
+
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
+    });
+    
+
+    conn.on('open', async () => {
+        var st = Session.createStringSession(conn.base64EncodedAuthInfo());
+        console.log(
+            chalk.green.bold('Smart Help String: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+        );
+        
+        if (!fs.existsSync('config.env')) {
+            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
+        }
+        if (conn.user.jid.startsWith('90')) {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*SMART*', MessageType.text)
+            console.log(
+                chalk.blue.bold('SMART.')
+            );
+        }
+        else {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!*', MessageType.text)
+            console.log(
+                chalk.blue.bold('If you are installing locale, you can start the bot with node SMART.js')
+            );
+        }
+        
+        process.exit(0);
+    });
+
+    await conn.connect();
+}
+
+SmartHelp()
+
